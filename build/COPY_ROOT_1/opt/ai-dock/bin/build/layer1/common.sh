4ddb0fae8494d896797f46ef558d19b908917fac
Yoonsoo Kim <laptise@live.jp>
2025-02-28 22:12:51 +0900

    run

--- build/COPY_ROOT_1/opt/ai-dock/bin/build/layer1/common.sh
+++ build/COPY_ROOT_1/opt/ai-dock/bin/build/layer1/common.sh
@@ -22,7 +22,6 @@ build_common_install_filebrowser()
 	filemanager_arch="unknown"
 	install_path="/opt/ai-dock/filebrowser"
 	mkdir -p $install_path
-	chmod -R 777 $install_path
 
 	# Termux on Android has $PREFIX set which already ends with /usr
 	if [[ -n "$ANDROID_ROOT" && -n "$PREFIX" ]]; then
